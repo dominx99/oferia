e94c6f2a90146780c78a8cba2f387505bcf3390d
dominx99 <dominx992@gmail.com>
2018-12-12 00:24:46 +0100

    Fix connection to database

--- app/Database/Migration.php
+++ app/Database/Migration.php
@@ -23,21 +23,7 @@ class Migration extends AbstractMigration
      */
     public function init(): void
     {
-        $capsule = new Manager();
-        $capsule->addConnection([
-            'driver'    => env('DATABASE_DRIVER', 'mysql'),
-            'host'      => env('DATABASE_HOST', 'localhost'),
-            'database'  => env('DATABASE_NAME', 'board'),
-            'username'  => env('DATABASE_USER', 'root'),
-            'password'  => env('DATABASE_PASS', ''),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => 'board_',
-        ]);
-        $capsule->setAsGlobal();
-        $capsule->bootEloquent();
-
-        $this->capsule = $capsule;
+        $this->capsule = new Manager();
         $this->schema  = $this->capsule->schema();
     }
 }
