7284a01841cc15db8c6ff5b4c0a2e3ac83b23b6c
dominx99 <dominx992@gmail.com>
2019-02-03 22:32:22 +0100

    Add property annonations to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property integer $id
+ * @property string $email
+ * @property string $password
+ */
 class User extends Model
 {
     /**
